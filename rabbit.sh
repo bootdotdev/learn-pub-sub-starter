d612d5b298fd2c951576714d5e075301d07adc32
wagslane <lane.c.wagner@gmail.com>
2024-04-18 13:59:29 -0600

    betterabbit

--- rabbit.sh
+++ rabbit.sh
@@ -1,1 +1,19 @@
-docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+#!/bin/bash
+
+case "$1" in
+    start)
+        echo "Starting RabbitMQ container..."
+        docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+        ;;
+    stop)
+        echo "Stopping RabbitMQ container..."
+        docker stop rabbitmq
+        ;;
+    logs)
+        echo "Fetching logs for RabbitMQ container..."
+        docker logs -f rabbitmq
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|logs}"
+        exit 1
+esac
