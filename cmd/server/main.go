bce58871456922465d81f30f8fafc57a3b710972
ntEOS <nguyentoan231196@gmail.com>
2024-05-07 22:32:44 +0700

    setup connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+
+	connection, err := amqp.Dial(connectionStr)
+	if err != nil {
+		panic(err)
+	}
+
+	defer connection.Close()
 }
