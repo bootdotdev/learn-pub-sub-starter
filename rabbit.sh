ab556548e8b788c85e6338ddfad32a102aed89f0
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-05-04 23:45:46 -0400

    Update RabbitMQ container version to 4.0.2 in startup script

--- rabbit.sh
+++ rabbit.sh
@@ -3,7 +3,7 @@
 case "$1" in
     start)
         echo "Starting RabbitMQ container..."
-        docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+        docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:4.0.2-management
         ;;
     stop)
         echo "Stopping RabbitMQ container..."
