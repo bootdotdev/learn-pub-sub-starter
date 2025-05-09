5cb034abfaa1d9a89ba10e5093f1ab3742a4a44f
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-05-08 23:52:29 -0400

    Implement RabbitMQ connection and queue declaration in client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %s\n", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to RabbitMQ")
+
+	username, err := gamelogic.ClientWelcome()
+
+	if err != nil {
+		log.Fatalf("Failed to welcome client: %s\n", err)
+	}
+	fmt.Printf("Welcome, %s!\n", username)
+
+	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
+
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		queueName,
+		routing.PauseKey,
+		int(pubsub.SimpleQueueType(1)),
+	)
+	if err != nil {
+		log.Fatalf("Failed to declare and bind queue: %s\n", err)
+	}
+
+	fmt.Printf("Queue %s declared and bound\n", queueName)
+
+	gamelogic.ClientWelcome()
 }
